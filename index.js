56274ab61f3b467c6bd81149b2dc65e800942cbe
ruhanaatiq <ruhana.atiq@gmail.com>
2025-06-03 20:22:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
